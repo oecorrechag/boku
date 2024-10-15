e9627899c0b74ddf04d852a2c7c6aaee94ea4edd
oecorrechag <oecorrechag@unal.edu.co>
2024-10-14 22:44:41 -0500

    new tester

--- extract.py
+++ extract.py
@@ -1,19 +1,31 @@
+import os
 import pandas as pd
-from sklearn.datasets import fetch_california_housing
 import datetime
+from sqlalchemy import create_engine
+from sklearn.datasets import fetch_california_housing
+
+from mypackage import read_table, upload_data_mysql
 
 ahora = datetime.datetime.now()
 print(f'Extrac - La fecha es: {ahora}')
-
-housing_data_raw = pd.read_csv('data/raw/housing_data_raw.csv', sep = ',', decimal = '.', header = 0, encoding = 'utf-8')
-print(f'shape old: {housing_data_raw.shape}')
 
 data = fetch_california_housing(as_frame=True)
 housing_data_new = data.frame
 housing_data_new = housing_data_new.sample(frac=0.005)
 
-df = pd.concat([housing_data_raw, housing_data_new], ignore_index=True)
+BD_KEY = os.getenv("BD_KEY")
 
-df.to_csv('data/raw/housing_data_raw.csv', encoding = 'utf-8-sig', index = False)
+engine = create_engine(f'mysql+pymysql://root:{BD_KEY}@localhost/houses')
 
-print(f'shape actual: {df.shape}')
+df_old = read_table(engine, 'raw')
+print(f'shape actual: {df_old.shape}')
+
+data = fetch_california_housing(as_frame=True)
+data_new = data.frame
+data_new = data_new.sample(frac=0.0005)
+print(f'shape data new: {data_new.shape}')
+
+df = pd.concat([df_old, data_new], ignore_index=True)
+print(f'shape new: {df.shape}')
+
+upload_data_mysql(engine, df, 'raw')
