7c2ab9b9f3844f300ecc52f1cdcafa1697681bd0
oecorrechag <oecorrechag@unal.edu.co>
2024-10-14 23:22:41 -0500

    U8

--- extract.py
+++ extract.py
@@ -6,8 +6,8 @@ from sklearn.datasets import fetch_california_housing
 
 from mypackage.connect import read_table, upload_data_mysql
 
-ahora = datetime.datetime.now()
-print(f'Extrac - La fecha es: {ahora}')
+# ahora = datetime.datetime.now()
+# print(f'Extrac - La fecha es: {ahora}')
 
 data = fetch_california_housing(as_frame=True)
 housing_data_new = data.frame
@@ -16,6 +16,9 @@ housing_data_new = housing_data_new.sample(frac=0.005)
 BD_KEY = os.getenv("BD_KEY")
 
 engine = create_engine(f'mysql+pymysql://root:{BD_KEY}@localhost/houses')
+
+ahora = datetime.datetime.now()
+print(f'Extrac - La fecha es: {ahora}')
 
 df_old = read_table(engine, 'raw')
 print(f'shape actual: {df_old.shape}')
