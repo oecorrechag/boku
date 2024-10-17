eca7058f2a48787d7a0915a0a240cf51eab7514e
oecorrechag <oecorrechag@unal.edu.co>
2024-10-17 08:13:31 -0500

    responsive para volver a la version estable

--- transform.py
+++ transform.py
@@ -5,14 +5,16 @@ import datetime
 ahora = datetime.datetime.now()
 print(f'Transform - La fecha es: {ahora}')
 
-housing_data_raw = pd.read_csv('data/raw/housing_data_raw.csv', sep = ',', decimal = '.', header = 0, encoding = 'utf-8')
+housing_data_raw = pd.read_csv('data/raw/housing_data_raw.csv', 
+                               sep = ',', decimal = '.', header = 0, encoding = 'utf-8')
 print(f'dim now housing_data_raw: {housing_data_raw.shape}')
 
 # Perform transformation
 housing_data_raw.rename(columns={'MedHouseVal': 'target'}, inplace=True)
 housing_data_raw['prediction'] = housing_data_raw['target'].values + np.random.normal(0, 5, housing_data_raw.shape[0])
 
-housing_data_clean = pd.read_csv('data/transform/housing_data_clean.csv', sep = ',', decimal = '.', header = 0, encoding = 'utf-8')
+housing_data_clean = pd.read_csv('data/transform/housing_data_clean.csv', 
+                                 sep = ',', decimal = '.', header = 0, encoding = 'utf-8')
 print(f'dim now housing_data_clean: {housing_data_clean.shape}')
 
 df = pd.concat([housing_data_raw, housing_data_clean], ignore_index=True)
